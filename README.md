deb8bc94343d81bb8ea7681852a1503438948d14
Noel Gaetan <gaetan.noel@viacesi.fr>
2021-10-16 23:32:59 +0200

    Port CircuitPython

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-## TB6612FNG_micropython
- Micropython code for TB6612FNG motor controller
+## CircuitPython_TB6612FNG
+CircuitPython code for TB6612FNG motor controller
 
-* Tested with ESP8266
-* Can be powered with 3S LiPo battery (Not tested with full charged(12.6v) battery)
-* Logic voltage 3.3v ESP8266 output
+* Tested with Pi Pico
+* Logic voltage 3.3v Pico output
--- fng.py
+++ fng.py
@@ -1,24 +1,31 @@
-
-from machine import PWM, Pin
+import time
+import board
+import pwmio
+from digitalio import DigitalInOut, Direction, Pull
 
 class Fng:
     def __init__(self, pwm_pin, cw_pin, ccw_pin):
         self.pwm_pin = pwm_pin
-        self.cw_pin = Pin(cw_pin, Pin.OUT)
-        self.ccw_pin = Pin(ccw_pin, Pin.OUT)
+        #self.cw_pin = Pin(cw_pin, Pin.OUT)
+        self.cw_pin = DigitalInOut(cw_pin)
+        self.cw_pin.direction = Direction.OUTPUT
+        #self.ccw_pin = Pin(ccw_pin, Pin.OUT)
+        self.ccw_pin = DigitalInOut(ccw_pin)
+        self.ccw_pin.direction = Direction.OUTPUT
 
-        self.motor = PWM(Pin(self.pwm_pin), freq=1000, duty=0)
+        #self.motor = PWM(Pin(self.pwm_pin), freq=1000, duty=0)
+        self.motor =  pwmio.PWMOut(pwm_pin, frequency=1000, duty_cycle=0)
         #begin.freq(1000) # Set PWM frequency 1000Hz
         #begin.duty(0)
 
     def write(self, pwm_in, dir):
         if dir == 'cw':
-            self.cw_pin.off() # Turns on cw_pin
-            self.ccw_pin.on() # Some nodemcu's wires connected in other way around 
+            self.cw_pin.value = True # Turns on cw_pin
+            self.ccw_pin.value = False # Some nodemcu's wires connected in other way around 
         elif dir == 'ccw':
-            self.cw_pin.on()
-            self.ccw_pin.off()
-        self.motor.duty(pwm_in)
+            self.cw_pin.value = False 
+            self.ccw_pin.value = True 
+        self.motor.duty_cycle = pwm_in
 
 if __name__ == '__main__':
     m1 = Fng(14, 0, 2)
